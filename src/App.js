5ec0b8ed7293c008f2098731e9272ab932d44ec4
Namgirl <seebachkim@gmail.com>
2023-01-24 16:13:33 +0200

    added new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Image</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Kim Timke</small>
+        </footer>
+      </div>
     </div>
   );
 }
