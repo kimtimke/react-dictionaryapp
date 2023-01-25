64bf0407de832afc82e375fa6b4454508808d09e
Namgirl <seebachkim@gmail.com>
2023-01-25 12:06:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ export default function App() {
             <a
               href="https://github.com/kimtimke/react-dictionaryapp"
               target="_blank"
+              rel="noreferrer"
             >
               Github.
             </a>
