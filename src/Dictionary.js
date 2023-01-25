9115ccc4fbe532ec2fb6f779c52365c1b0c5ba67
Namgirl <seebachkim@gmail.com>
2023-01-25 11:49:41 +0200

    rendered, displayed the word meanings and definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -19,12 +21,13 @@ export default function Dictionary() {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
+  //every time the state changes, it will be recompiled with new data
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Meanings from "./Meanings";
+
+export default function Results(props) {
+  console.log(props.result);
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meanings meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
