63915fafa7813f5b3fde142f5f74ecc51c74d298
Namgirl <seebachkim@gmail.com>
2023-01-28 17:59:11 +0200

    added Synonyms to definitions

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
+  console.log(props.meaning);
   return (
     <div className="Meanings">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <strong>Definition:</strong>
+            {definition.definition}
+            <br />
+            <em>{definition.example}</em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
