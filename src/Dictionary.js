b25930ceaa066f639644d814c79c431275a268f6
Namgirl <seebachkim@gmail.com>
2023-01-29 13:50:21 +0200

    improved UI + added styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -36,8 +36,13 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h1>What do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
 
           <div className="hint">suggested words: thunder, dry, love ...</div>
